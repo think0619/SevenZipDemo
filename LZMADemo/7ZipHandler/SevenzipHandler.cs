bad75bdb2694f29304f682d9964266d5c6db4ef8
think <thinkxs@outlook.com>
2020-09-30 22:34:12 +0800

    add compress and extral demo

---
+++ LZMADemo/7ZipHandler/SevenzipHandler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace LZMADemo._7ZipHandler
+{
+    public class SevenzipHandler
+    {
+        /// <summary>
+        /// Compress file to 7Z by 7za.exe
+        /// </summary>
+        /// <param name="sevenzaPath"></param>
+        /// <param name="sourceFilePath"></param>
+        /// <param name="objectDirectorPath"></param>
+        public static void CompressFile(string sevenzaPath, string sourceFilePath, string objectDirectorPath)
+        {
+            if (File.Exists(String.Format(sevenzaPath)) && File.Exists(sourceFilePath))
+            {
+                ProcessStartInfo progress = new ProcessStartInfo();
+                progress.FileName = sevenzaPath;
+                progress.Arguments = string.Format($@"a -t7z {String.Format($"{objectDirectorPath}\\{Path.GetFileNameWithoutExtension(sourceFilePath)}.7z")} {sourceFilePath} -mx=9 -aoa");
+                Debug.WriteLine(progress.Arguments);
+                progress.WindowStyle = ProcessWindowStyle.Hidden;
+                System.Diagnostics.Process compressthread = System.Diagnostics.Process.Start(progress);
+                compressthread.WaitForExit();
+            }
+        }
+
+        /// <summary>
+        /// Extral Zip File
+        /// </summary>
+        /// <param name="sevenzaPath"></param>
+        /// <param name="sourceFilePath"></param>
+        /// <param name="objectDirecotrPath"></param>
+        public static void ExtraZipFile(string sevenzaPath,string sourceFilePath,string objectDirecotrPath)
+        {
+            if (File.Exists(String.Format(sevenzaPath)) && File.Exists(sourceFilePath))
+            {
+                if (!Directory.Exists(objectDirecotrPath))
+                {
+                    Directory.CreateDirectory(objectDirecotrPath);
+                }
+                ProcessStartInfo progress = new ProcessStartInfo();
+                progress.FileName = sevenzaPath;
+                progress.Arguments = string.Format($@" e {sourceFilePath} -o{objectDirecotrPath} -aoa ");
+                progress.WindowStyle = ProcessWindowStyle.Hidden;
+                System.Diagnostics.Process compressthread = System.Diagnostics.Process.Start(progress);
+                compressthread.WaitForExit();
+            } 
+        }
+    }
+}
---
+++ LZMADemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using LZMADemo._7ZipHandler;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LZMADemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sevenZipFolderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"7ZipHandler\7-Zip\7za.exe");
+
+            string sourceFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"TestFile\简明py教程.pdf"); 
+            SevenzipHandler.CompressFile(sevenZipFolderPath, sourceFilePath, Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"TestFile\ZipFolder"));
+           // System.Threading.Thread.Sleep(5000);
+            SevenzipHandler.ExtraZipFile(
+                sevenZipFolderPath, 
+                Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"TestFile\ZipFolder\简明py教程.7z"),
+                Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"TestFile\ZipFolder"));
+        }
+    }
+}
